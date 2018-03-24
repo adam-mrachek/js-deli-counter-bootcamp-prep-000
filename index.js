9f6e179856a1a745e7c1c40ca20bf66dd4d05502
Adam Mrachek <amrachek@outlook.com>
2018-03-24 18:28:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function takeANumber(katzDeliLine, name) {
 
 function nowServing(deliLine) {
   if (deliLine.length > 0) {
-    return deliLine.shift();
+    return `Currently serving ${deliLine.shift()}`;
   } else {
     return "There is nobody waiting to be served!";
   }
