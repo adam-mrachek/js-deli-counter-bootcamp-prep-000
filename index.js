3f113315905dbc0981b4a177f501b131b57137e1
Adam Mrachek <amrachek@outlook.com>
2018-03-24 18:27:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name) + 1} in line.`;
 }
+
+function nowServing(deliLine) {
+  if (deliLine.length > 0) {
+    return deliLine.shift();
+  } else {
+    return "There is nobody waiting to be served!";
+  }
+}
