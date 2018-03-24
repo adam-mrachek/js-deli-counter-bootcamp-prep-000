c0f6aea72fc47cffa3233025d7062fc73df3f95b
Adam Mrachek <amrachek@outlook.com>
2018-03-24 18:22:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,6 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name) + 1} in line.`;
+}
